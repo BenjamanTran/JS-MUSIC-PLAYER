c6c0ae76966f8e98fd69fb5b447758b7a27691a7
benjaman tran <t16021999@gmail.com>
2023-08-10 16:05:04 +0700

    fix for some link die and use 1 src for load every music

--- main.js
+++ main.js
@@ -2,39 +2,47 @@ const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
 const playList = $('.playlist')
+const cd = $('.cd')
+const songName = $('header h2')
+const imgSong = $('.cd-thumb')
+const audioSong = $('#audio')
+const btnPlay = $('.btn-toggle-play')
+const player = $('.player')
 const app = {
+    isPlaying: false,
+    currentIndex: 0,
     songs: [
-        {
-            name: "Click Pow Get Down",
-            singer: "Raftaar x Fortnite",
-            path: "https://mp3.vlcmusic.com/download.php?track_id=34737&format=320",
-            image: "https://i.ytimg.com/vi/jTLhQf5KJSc/maxresdefault.jpg"
-        },
         {
             name: "Tu Phir Se Aana",
             singer: "Raftaar x Salim Merchant x Karma",
-            path: "https://mp3.vlcmusic.com/download.php?track_id=34213&format=320",
+            path: "https://mp3.filmisongs.com/go.php?id=Damn%20Song%20Raftaar%20Ft%20KrSNa.mp3",
             image:
                 "https://1.bp.blogspot.com/-kX21dGUuTdM/X85ij1SBeEI/AAAAAAAAKK4/feboCtDKkls19cZw3glZWRdJ6J8alCm-gCNcBGAsYHQ/s16000/Tu%2BAana%2BPhir%2BSe%2BRap%2BSong%2BLyrics%2BBy%2BRaftaar.jpg"
+        },
+        {
+            name: "Click Pow Get Down",
+            singer: "Raftaar x Fortnite",
+            path: "https://mp3.filmisongs.com/go.php?id=Damn%20Song%20Raftaar%20Ft%20KrSNa.mp3",
+            image: "https://i.ytimg.com/vi/jTLhQf5KJSc/maxresdefault.jpg"
         },
         {
             name: "Naachne Ka Shaunq",
             singer: "Raftaar x Brobha V",
             path:
-                "https://mp3.filmysongs.in/download.php?id=Naachne Ka Shaunq Raftaar Ft Brodha V Mp3 Hindi Song Filmysongs.co.mp3",
+                "https://mp3.filmisongs.com/go.php?id=Damn%20Song%20Raftaar%20Ft%20KrSNa.mp3",
             image: "https://i.ytimg.com/vi/QvswgfLDuPg/maxresdefault.jpg"
         },
         {
             name: "Mantoiyat",
             singer: "Raftaar x Nawazuddin Siddiqui",
-            path: "https://mp3.vlcmusic.com/download.php?track_id=14448&format=320",
+            path: "https://mp3.filmisongs.com/go.php?id=Damn%20Song%20Raftaar%20Ft%20KrSNa.mp3",
             image:
                 "https://a10.gaanacdn.com/images/song/39/24225939/crop_480x480_1536749130.jpg"
         },
         {
             name: "Aage Chal",
             singer: "Raftaar",
-            path: "https://mp3.vlcmusic.com/download.php?track_id=25791&format=320",
+            path: "https://mp3.filmisongs.com/go.php?id=Damn%20Song%20Raftaar%20Ft%20KrSNa.mp3",
             image:
                 "https://a10.gaanacdn.com/images/albums/72/3019572/crop_480x480_3019572.jpg"
         },
@@ -49,11 +57,18 @@ const app = {
         {
             name: "Feeling You",
             singer: "Raftaar x Harjas",
-            path: "https://mp3.vlcmusic.com/download.php?track_id=27145&format=320",
+            path: "https://mp3.filmisongs.com/go.php?id=Damn%20Song%20Raftaar%20Ft%20KrSNa.mp3",
             image:
                 "https://a10.gaanacdn.com/gn_img/albums/YoEWlabzXB/oEWlj5gYKz/size_xxl_1586752323.webp"
         }
     ],
+    defineProperties: function () {
+        Object.defineProperty(this, 'currentSong', {
+            get: function () {
+                return this.songs[this.currentIndex]
+            }
+        })
+    },
     render: function () {
         const htmls = this.songs.map(song => {
             return `
@@ -79,8 +94,9 @@ const app = {
         playList.innerHTML = htmls.join('')
     },
     handleEvenScroll: function () {
-        const cd = $('.cd')
         const cdWidth = cd.offsetWidth
+        const self = this
+
         document.onscroll = function () {
             const scrollTop = window.scrollY || document.documentElement.scrollTop
             const newWidth = cdWidth - scrollTop
@@ -88,10 +104,32 @@ const app = {
             cd.style.width = newWidth > 0 ? newWidth + 'px' : 0
             cd.style.opacity = newWidth > 54 ? newWidth / cdWidth : 0
         }
+
+        btnPlay.onclick = function () {
+            self.isPlaying ? audioSong.pause() : audioSong.play()
+        }
+        audioSong.onplay = function () {
+            self.isPlaying = true
+            player.classList.add('playing')
+        }
+        audioSong.onpause = function () {
+            self.isPlaying = false
+            player.classList.remove('playing')
+        }
+    },
+    loadCurrentSong: function () {
+        songName.textContent = this.currentSong.name
+        imgSong.style.backgroundImage = `url('${this.currentSong.image}')`
+        audioSong.src = this.currentSong.path
     },
     start: function () {
+        this.defineProperties()
         this.render()
         this.handleEvenScroll()
+        this.loadCurrentSong()
     }
 }
 app.start();
+
+
+
