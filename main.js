54bc8fa1f90219d41c9598030320b76b3b1c4e0a
Tan Tran <tantt@zigexn.vn>
2023-08-10 14:10:53 +0700

    done show data song with js

---
+++ main.js
@@ -0,0 +1,85 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const playList = $('.playlist')
+const app = {
+    songs: [
+        {
+            name: "Click Pow Get Down",
+            singer: "Raftaar x Fortnite",
+            path: "https://mp3.vlcmusic.com/download.php?track_id=34737&format=320",
+            image: "https://i.ytimg.com/vi/jTLhQf5KJSc/maxresdefault.jpg"
+        },
+        {
+            name: "Tu Phir Se Aana",
+            singer: "Raftaar x Salim Merchant x Karma",
+            path: "https://mp3.vlcmusic.com/download.php?track_id=34213&format=320",
+            image:
+                "https://1.bp.blogspot.com/-kX21dGUuTdM/X85ij1SBeEI/AAAAAAAAKK4/feboCtDKkls19cZw3glZWRdJ6J8alCm-gCNcBGAsYHQ/s16000/Tu%2BAana%2BPhir%2BSe%2BRap%2BSong%2BLyrics%2BBy%2BRaftaar.jpg"
+        },
+        {
+            name: "Naachne Ka Shaunq",
+            singer: "Raftaar x Brobha V",
+            path:
+                "https://mp3.filmysongs.in/download.php?id=Naachne Ka Shaunq Raftaar Ft Brodha V Mp3 Hindi Song Filmysongs.co.mp3",
+            image: "https://i.ytimg.com/vi/QvswgfLDuPg/maxresdefault.jpg"
+        },
+        {
+            name: "Mantoiyat",
+            singer: "Raftaar x Nawazuddin Siddiqui",
+            path: "https://mp3.vlcmusic.com/download.php?track_id=14448&format=320",
+            image:
+                "https://a10.gaanacdn.com/images/song/39/24225939/crop_480x480_1536749130.jpg"
+        },
+        {
+            name: "Aage Chal",
+            singer: "Raftaar",
+            path: "https://mp3.vlcmusic.com/download.php?track_id=25791&format=320",
+            image:
+                "https://a10.gaanacdn.com/images/albums/72/3019572/crop_480x480_3019572.jpg"
+        },
+        {
+            name: "Damn",
+            singer: "Raftaar x kr$na",
+            path:
+                "https://mp3.filmisongs.com/go.php?id=Damn%20Song%20Raftaar%20Ft%20KrSNa.mp3",
+            image:
+            "https://media.istockphoto.com/id/682516816/vi/vec-to/t%E1%BA%A3i-cu%E1%BB%91i-tu%E1%BA%A7n-minh-h%E1%BB%8Da-vector-n%E1%BB%81n-m%C3%A0u-v%C3%A0ng.jpg?s=2048x2048&w=is&k=20&c=_ylwoLvSOx27qd1wjoxtETI7wwFQC1643nfeuOkS8xE="
+        },
+        {
+            name: "Feeling You",
+            singer: "Raftaar x Harjas",
+            path: "https://mp3.vlcmusic.com/download.php?track_id=27145&format=320",
+            image:
+                "https://a10.gaanacdn.com/gn_img/albums/YoEWlabzXB/oEWlj5gYKz/size_xxl_1586752323.webp"
+        }
+    ],
+    render: function () {
+        const htmls = this.songs.map(song => {
+            return `
+        
+        <div class="song">
+          <div
+            class="thumb"
+            style="
+              background-image: url('${song.image}');
+            "
+          ></div>
+          <div class="body">
+            <h3 class="title">${song.name}</h3>
+            <p class="author">${song.singer}</p>
+          </div>
+          <div class="option">
+            <i class="fas fa-ellipsis-h"></i>
+          </div>
+        </div>
+    
+        `
+        })
+        playList.innerHTML=htmls.join('')
+    },
+    start: function () {
+        this.render()
+    }
+}
+app.start();
